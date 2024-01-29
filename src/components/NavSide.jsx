d794fbd212d319686cbe7b7e82dc636c56e3a0d1
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2024-01-29 12:54:54 +0530

    Add files via upload

--- src/components/NavSide.jsx
+++ src/components/NavSide.jsx
@@ -4,14 +4,14 @@ import "../styles/navside.scss"; // Assuming SCSS is configured for React
 function NavSide() {
   const [isClose, setIsClose] = useState(true);
   const [isHoverable, setIsHoverable] = useState(false);
-
+  const [showExpandSidebar, setShowExpandSidebar] = useState(true);
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 768) {
-        setIsClose(true);
-      } else {
-        setIsClose(false);
-      }
+      // if (window.innerWidth < 768) {
+      //   setIsClose(true);
+      // } else {
+      //   setIsClose(false);
+      // }
     };
 
     window.addEventListener("resize", handleResize);
@@ -35,6 +35,9 @@ function NavSide() {
       setIsClose(true);
     }
   };
+const handleBottomContentClick = () => {
+  setShowExpandSidebar(!showExpandSidebar); // Toggle bottom content visibility
+};
 
   return (
     <nav
@@ -43,55 +46,72 @@ function NavSide() {
       onMouseLeave={handleSidebarMouseLeave}
     >
       <ul class="menu_items">
-          <div class="menu_title menu_editor"></div>
-          <li class="item">
-            <a href="#" class="nav_link">
-              <span class="navlink_icon">
-                <i class="bx bxs-magic-wand"></i>
-              </span>
-              <span class="navlink">Magic build</span>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#" class="nav_link">
-              <span class="navlink_icon">
-                <i class="bx bx-loader-circle"></i>
-              </span>
-              <span class="navlink">Filters</span>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#" class="nav_link">
-              <span class="navlink_icon">
-                <i class="bx bx-filter"></i>
-              </span>
-              <span class="navlink">Filter</span>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#" class="nav_link">
-              <span class="navlink_icon">
-                <i class="bx bx-cloud-upload"></i>
-              </span>
-              <span class="navlink">Upload new</span>
-            </a>
-          </li>
-        </ul>
-      <div className="bottom_content">
-        <div className="bottom expand_sidebar" onClick={handleSidebarToggle}>
-          <span>Expand</span>
-          <i className="bx bx-log-in"></i>
-        </div>
-        <div
-          className="bottom collapse_sidebar"
-          onClick={() => {
-            setIsClose(true);
-            setIsHoverable(true);
-          }}
-        >
-          <span>Collapse</span>
-          <i className="bx bx-log-out"></i>
-        </div>
+        <li class="item">
+          <a href="#" class="nav_link">
+            <span class="navlink_icon">
+              <i class="bx bxs-magic-wand"></i>
+            </span>
+            <span class="navlink">Translation</span>
+          </a>
+        </li>
+        <li class="item">
+          <a href="#" class="nav_link">
+            <span class="navlink_icon">
+              <i class="bx bx-loader-circle"></i>
+            </span>
+            <span class="navlink">Conversion</span>
+          </a>
+        </li>
+        <li class="item">
+          <a href="#" class="nav_link">
+            <span class="navlink_icon">
+              <i class="bx bx-filter"></i>
+            </span>
+            <span class="navlink">Home</span>
+          </a>
+        </li>
+        <li class="item">
+          <a href="#" class="nav_link">
+            <span class="navlink_icon">
+              <i class="bx bx-cloud-upload"></i>
+            </span>
+            <span class="navlink">Learning</span>
+          </a>
+        </li>
+        <li class="item">
+          <a href="#" class="nav_link">
+            <span class="navlink_icon">
+              <i class="bx bx-cloud-upload"></i>
+            </span>
+            <span class="navlink">Profile</span>
+          </a>
+        </li>
+      </ul>
+      <div className="bottom_content" onClick={handleBottomContentClick}>
+        {showExpandSidebar && (
+          <div
+            className="bottom expand_sidebar"
+            onClick={() => {
+              setIsClose(false);
+              setIsHoverable(false);
+            }}
+          >
+            <span>Expand</span>
+            <i className="bx bx-log-in"></i>
+          </div>
+        )}
+        {!showExpandSidebar && (
+          <div
+            className="bottom collapse_sidebar"
+            onClick={() => {
+              setIsClose(true);
+              setIsHoverable(true);
+            }}
+          >
+            <span>Collapse</span>
+            <i className="bx bx-log-out"></i>
+          </div>
+        )}
       </div>
     </nav>
   );
