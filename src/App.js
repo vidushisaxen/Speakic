9c684e932fd2c9ae48fb5fb3f232debf5d2e1f06
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-02-04 17:20:11 +0530

    update1.1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Login from './components/login';
-import NavSide from './components/NavSide'
+import NavSide from './components/NavSide';
+import NavTop from './components/NavTop'
 function App() {
   return (
    <>
    <NavSide/>
+   <NavTop/>
    </>
   );
 }
