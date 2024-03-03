490916ea2af16a838a97dbdeeb9d61a9041167ec
Shivanshika <shivanshikaagarwal7275@gmail.com>
2024-03-03 14:57:09 +0530

    Contact Us

--- src/components/contactus.jsx
+++ src/components/contactus.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/contactus.scss";
 import logo from "../Assets/logo.jpg"
 const ContactUs= ()=>{
+ function Accordion({ title, content }) {
+   const [isOpen, setIsOpen] = useState(false);
+
+   const handleClick = () => {
+     setIsOpen(!isOpen);
+   };
+
+   return (
+     <div className="accordion-item">
+       <button className="accordion-title" onClick={handleClick}>
+         {title}
+         <lord-icon
+           src="https://cdn.lordicon.com/rmkahxvq.json"
+           trigger="hover"
+           state="hover-arrow-down-2"
+           colors="primary:#033431"
+         ></lord-icon>
+       </button>
+       {isOpen && <div className="accordion-content">{content}</div>}
+     </div>
+   );
+ }
+  const sections = [
+    { title: "Is the Conversion free?", content: "Yes, you can use this feature for free." },
+    { title: "Do you provide tutorials?", content: "Our help section is very extensive. You can also contact our team for any querry." },
+    { title: "How does support work?", content: "You can e-mail our team with your questions. They will be answered at the first opportunity. You can also call us with the given number from Monday to Friday during 10am to 5pm." },
+  ];
      return (
        <>
          <div className="body">
            <img src={logo} className="logo rounded-full" alt="Speakic" />
-           <div className="text-5xl leading-10 heading">Contact our friendly team</div>
+           <div className="text-5xl leading-10 heading">
+             Contact our friendly team
+           </div>
            <div className="text-xl leading-10">
              Let us know how we can help.
            </div>
@@ -82,6 +111,20 @@ const ContactUs= ()=>{
                    +91 7275503320
                  </a>
                </div>
+             </div>
+           </div>
+           <div>
+             <div className="text-5xl leading-10 heading">
+               Frequently Asked Questions
+             </div>
+             <div className="accordion">
+               {sections.map((section) => (
+                 <Accordion
+                   key={section.title}
+                   title={section.title}
+                   content={section.content}
+                 />
+               ))}
              </div>
            </div>
          </div>
